9aabe836758122b677056f70a3427756a62a8ad0
saleroa <2985299550@qq.com>
2023-08-08 00:06:18 +0800

    小修改

--- server/favor/rpc/internal/svc/servicecontext.go
+++ server/favor/rpc/internal/svc/servicecontext.go
@@ -14,34 +14,9 @@ type ServiceContext struct {
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	// 尚未完善
 	cnf := redis.Options{
-		Network:               "",
-		Addr:                  "",
-		ClientName:            "",
-		Dialer:                nil,
-		OnConnect:             nil,
-		Protocol:              0,
-		Username:              "",
-		Password:              "",
-		CredentialsProvider:   nil,
-		DB:                    0,
-		MaxRetries:            0,
-		MinRetryBackoff:       0,
-		MaxRetryBackoff:       0,
-		DialTimeout:           0,
-		ReadTimeout:           0,
-		WriteTimeout:          0,
-		ContextTimeoutEnabled: false,
-		PoolFIFO:              false,
-		PoolSize:              0,
-		PoolTimeout:           0,
-		MinIdleConns:          0,
-		MaxIdleConns:          0,
-		ConnMaxIdleTime:       0,
-		ConnMaxLifetime:       0,
-		TLSConfig:             nil,
-		Limiter:               nil,
+		Addr:     c.RedisConf.Host,
+		Password: "",
 	}
 	return &ServiceContext{
 		Config:     c,
